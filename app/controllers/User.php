fdaa8f29c8946e878587eeb41de8368eb26021db
Ngurah Prihandana <prihandana27@gmail.com>
2020-02-26 10:16:27 +0800

    Merubah file template bagi user

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -7,7 +7,7 @@
         {
             $data['judul'] = "Welcome Admin";
             $this->view('templates/header',$data);
-            $this->view('user/sidebar',$data);
+            $this->view('user/template/sidebar',$data);
             $this->view('templates/topbar',$data);
             $this->view('user/index',$data);
             $this->view('templates/footer',$data);
